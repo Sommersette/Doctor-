915cb374c0172bf6f73ddb47d5d012349985860f
Nick Powell and Trevor Fast <nick@>
2017-05-02 16:03:39 -0700

    add and pass spec test for doc_id.

--- spec/patient_spec.rb
+++ spec/patient_spec.rb
@@ -14,45 +14,45 @@ describe(Patient) do
 
   describe("#save") do
     it("adds a patient to the array of patients") do
-      patient = Patient.new({:name => "Dorf",:doc_id => 0, :birthdate => '2017-05-01 00:00:00'})
+      patient = Patient.new({:name => "Dorf",:doc_id => 0, :birthdate => '2017-05-01'})
       patient.save()
       expect(Patient.all()).to(eq([patient]))
     end
   end
 
-
   describe("name") do
     it("lets you see the name") do
-      patient = Patient.new({:name => "Dorf",:doc_id => 0, :birthdate => '2017-05-01 00:00:00'})
+      patient = Patient.new({:name => "Dorf",:doc_id => 0, :birthdate => '2017-05-01'})
       expect(patient.name()).to(eq("Dorf"))
     end
   end
 
   describe("#id") do
     it("lets you read the patient ID") do
-      patient = Patient.new({:name => "Dorf",:doc_id => 0, :birthdate => '2017-05-01 00:00:00'})
+      patient = Patient.new({:name => "Dorf",:doc_id => 0, :birthdate => '2017-05-01'})
       expect(patient.id()).to(eq(nil))
     end
   end
 
   describe("#birthdate") do
     it("lets you read the patient's birthdate") do
-      patient = Patient.new({:name => "Dorf",:doc_id => 0, :birthdate => '2017-05-01 00:00:00'})
-      expect(patient.id()).to(eq(nil))
+      patient = Patient.new({:name => "Dorf",:doc_id => 0, :birthdate => '2017-05-01'})
+      expect(patient.birthdate()).to(eq('2017-05-01'))
     end
   end
 
   describe("#==") do
     it("compares two patients") do
-      patient1 = Patient.new({:name => "Dorf",:doc_id => 0, :birthdate => '2017-05-01 00:00:00'})
-      patient2 = Patient.new({:name => "Dorf",:doc_id => 0, :birthdate => '2017-05-01 00:00:00'})
+      patient1 = Patient.new({:name => "Dorf",:doc_id => 0, :birthdate => '2017-05-01'})
+      patient2 = Patient.new({:name => "Dorf",:doc_id => 0, :birthdate => '2017-05-01'})
       expect(patient1).to(eq(patient2))
     end
   end
 
   describe("#doc_id") do
-    it("adds a doctors ID to the patient as foreign key.") do
-      
+    it(" lets you view a doctor's ID on each patient") do
+      patient = Patient.new({:name => "Dorf",:doc_id => 9, :birthdate => '2017-05-01'})
+      expect(patient.doc_id).to(eq(9))
     end
   end
 
